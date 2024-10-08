2d307c7a7accc278eef6fc46d2ab826d32445195
Sesan Joel <w0836046@myscc.ca>
2024-10-08 11:47:21 -0400

    A New Service Created - Moved data to a new typescript file called mock-contents.ts - Created a new service called countries

--- src/app/country-list/country-list.component.ts
+++ src/app/country-list/country-list.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { CountryListItemComponent } from '../country-list-item/country-list-item.component';
-import { Country } from '../models/country';
 import { NgFor } from '@angular/common';
+import { country } from '../data/mock-contents';
 
 @Component({
   selector: 'app-country-list',
@@ -11,30 +11,5 @@ import { NgFor } from '@angular/common';
   styleUrl: './country-list.component.scss',
 })
 export class CountryListComponent {
-  country: Country[] = [
-    {
-      name: 'Canada',
-      population: '38,000,000 Milion',
-      gdp: '$2.1 Trillion',
-      rating: '7/10',
-    },
-    {
-      name: 'Nigeria',
-      population: '218,000,000 Milion',
-      gdp: '$472 Billion',
-      rating: '5/10',
-    },
-    {
-      name: 'United States',
-      population: '333,000,000 Milion',
-      gdp: '$25 Trillion',
-      rating: '7/10',
-    },
-    {
-      name: 'Singapore',
-      population: '5.6,000,000 Milion',
-      gdp: '$466 Billion',
-      rating: '9/10',
-    },
-  ];
+  country = country;
 }
---
+++ src/app/data/mock-contents.ts
@@ -0,0 +1,28 @@
+import { Country } from '../models/country';
+
+export const country: Country[] = [
+  {
+    name: 'Canada',
+    population: '38,000,000 Milion',
+    gdp: '$2.1 Trillion',
+    rating: '7/10',
+  },
+  {
+    name: 'Nigeria',
+    population: '218,000,000 Milion',
+    gdp: '$472 Billion',
+    rating: '5/10',
+  },
+  {
+    name: 'United States',
+    population: '333,000,000 Milion',
+    gdp: '$25 Trillion',
+    rating: '7/10',
+  },
+  {
+    name: 'Singapore',
+    population: '5.6,000,000 Milion',
+    gdp: '$466 Billion',
+    rating: '9/10',
+  },
+];
