ea9e2dbbda64ad5292a9c88ac06cd2102273977d
Sesan Joel <w0836046@myscc.ca>
2024-10-08 20:13:54 -0400

    Routes Defined

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,16 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { provideRouter, Routes } from '@angular/router';
+import { CountryListComponent } from './app/country-list/country-list.component';
+import { CountryListItemComponent } from './app/country-list-item/country-list-item.component';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+const routes: Routes = [
+  { path: '', redirectTo: '/countries', pathMatch: 'full' },
+  { path: 'countries', component: CountryListComponent },
+  { path: 'country/:name', component: CountryListItemComponent },
+];
+
+bootstrapApplication(AppComponent, {
+  providers: [provideRouter(routes)],
+});
